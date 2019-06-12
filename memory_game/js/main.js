bf7054a9af3d6a17e9fd00cccb556bf70ae008ef
Justin Waltz <justinwaltz@Justins-iMac.local>
2019-06-12 16:11:53 -0500

    created game logic

---
+++ memory_game/js/main.js
@@ -0,0 +1,7 @@
+console.log("Up and running");
+var cardOne = "queen";
+var cardTwo = "king";
+var cardThree = "queen";
+var cardFour = "king";
+console.log("User Flipped " + cardTwo);
+console.log("User Flipped " + cardThree);
