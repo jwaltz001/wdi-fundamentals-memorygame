d4bb6726c0776275a4652f41c19f30fb765516de
Justin Waltz <justinwaltz@Justins-iMac.local>
2019-06-13 17:17:26 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,7 +1,17 @@
 console.log("Up and running");
-var cardOne = "queen";
-var cardTwo = "king";
-var cardThree = "queen";
-var cardFour = "king";
-console.log("User Flipped " + cardTwo);
-console.log("User Flipped " + cardThree);
+//console.log("User Flipped " + cardTwo);
+//console.log("User Flipped " + cardThree);
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
+console.log("user flipped queen");
+var cardTwo = cards[3]
+cardsInPlay.push(cardTwo)
+console.log("user flipped king")
+
+if (cardsInPlay.length === 2) {
+	} else if (cardsInPlay[0] === cardsInPlay[1]) {
+		alert("You found a match!");
+	} else {
+		alert("Sorry, try again.");
+	}
+
