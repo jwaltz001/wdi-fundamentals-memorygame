12ac470ab382f7e91bfa218295b29546ce7e49d9
Justin Waltz <justinwaltz@Justins-iMac.local>
2019-06-14 15:32:56 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,17 +1,39 @@
 console.log("Up and running");
 //console.log("User Flipped " + cardTwo);
 //console.log("User Flipped " + cardThree);
+//Old code to compare to functions
+	/*var cardOne =[0];
+	cardsInPlay.push(cardOne);
+	console.log("user flipped " + cardOne);
+	
+	var cardTwo = cards[2]
+	cardsInPlay.push(cardTwo);
+	console.log("user flipped " + cardTwo); 
+//my first try 
+	if (cardsInPlay.length === 2) {
+		} else if (cardsInPlay[0] === cardsInPlay[1]) {
+		alert("You found a match!");
+		} else {
+		alert("Sorry, try again.");
+		}
+	*/
+
+
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
-console.log("user flipped queen");
-var cardTwo = cards[3]
-cardsInPlay.push(cardTwo)
-console.log("user flipped king")
-
-if (cardsInPlay.length === 2) {
-	} else if (cardsInPlay[0] === cardsInPlay[1]) {
-		alert("You found a match!");
-	} else {
-		alert("Sorry, try again.");
+function checkForMatch () {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		console.log("You found a match!");
+		} else {
+		console.log("Sorry, try again.");
+		}
+}
+function flipCard(cardId) {
+	console.log("User flipped " + cards[cardId]);
+	cardsInPlay.push(cards[cardId]);
+	if (cardsInPlay.length === 2) {
+		checkForMatch()
 	}
-
+};
+flipCard(0);
+flipCard(2);
